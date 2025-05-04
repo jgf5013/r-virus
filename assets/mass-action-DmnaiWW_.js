f2097b2db4376596364f788271dee0b2daa67430
jgf5013 <jgf5013@gmail.com>
2025-05-04 18:08:18 +0000

    build: Deploy to GitHub Pages

---
+++ assets/mass-action-DmnaiWW_.js
@@ -0,0 +1,29 @@
+const n=`library(PBSddesolve)
+library(escape2024)
+
+print("Calling model_reference...")
+
+result = tryCatch({
+  data <- model_reference(
+    transmission_rate = 0.25,
+    infectiousness_rate = 0.25,
+    recovery_rate = 0.2,
+    time_end = 400,
+    increment = 1,
+    population_size = 20E6,
+    seed_infected = 1E-3
+  )
+  print(data)
+  flush.console()
+}, warning = function(w) {
+  print("Warning...")
+  print(w)
+}, error = function(e) {
+  print("Error...")
+  print(e)
+}, finally = {
+  print("All done!")
+  flush.console()
+})
+
+# json_output <- jsonlite::toJSON(data, pretty = TRUE)`;export{n as default};
